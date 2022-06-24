688b6ca00e65fb42b2f48eb6c4fb821bd61a7f73
antonio00727 <jmarrero3483@gmail.com>
2022-06-24 09:47:36 -0400

    changed character for player

--- js/index.js
+++ js/index.js
@@ -13,9 +13,9 @@ class Player{
         }
 
         const image=new Image()
-        image.src='./Image/Player.png'
+        image.src='./Image/pngwing.com.png'
         image.onload=()=>{
-            const scale=0.15
+            const scale=0.08
             this.image=image
             this.width=image.width *scale
             this.height=image.height *scale
