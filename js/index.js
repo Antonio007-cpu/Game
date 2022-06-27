24743f77d461adec6727ad9fd2850d76b13c3134
antonio00727 <jmarrero3483@gmail.com>
2022-06-27 01:35:46 -0400

    Game Finished

--- js/index.js
+++ js/index.js
@@ -47,21 +47,117 @@ class Player{
     }
 }
 
-class Projectile{
-    constructor(position){
+class Projectile {
+    constructor({position, velocity}){
         this.position=position
-        this.velocity=20
+        this.velocity=velocity
 
+        this.radius=4
     }
-    draw(){
-    context.beginPath()
-    context.arc(this.position.x,this.position.y,this.radios,0,Math.PI*2)
-    context.fillstyle='green'
 
+    draw(){
+        context.beginPath()
+        context.arc(this.position.x ,this.position.y ,this.radius , 0, Math.PI * 2)
+        context.fillStyle = 'green'
+        context.fill()
+        context.closePath()
+    }
+
+    update(){
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
     }
 
 }
+
+class Virus{
+    constructor({position}){
+        this.velocity={
+            x:0,
+            y:0
+        }
+
+        const image=new Image()
+        image.src='./Image/virus.png'
+        image.onload=()=>{
+            const scale=0.08
+            this.image=image
+            this.width=image.width *scale
+            this.height=image.height *scale
+            this.position={
+                x: position.x,
+                y: position.y
+            }
+        }
+
+       
+    }
+
+    draw(){
+        // context.fillStyle='green'
+        // context.fillRect(this.position.x, this.position.y, this.width, this.height)
+        if (this.image)
+            context.drawImage(
+                this.image,
+                this.position.x,
+                this.position.y,
+                this.width,
+                this.height)
+    }
+    update({velocity}){
+        if (this.image){
+        this.draw()
+        this.position.x += velocity.x
+        this.position.y += velocity.y
+        }
+    }
+}
+
+class Grid{
+    constructor(){
+        this.position ={
+            x:0,
+            y:0
+        }
+
+        this.velocity ={
+            x:3,
+            y:0
+        }
+        this.virus =[]
+
+        const columns = Math.floor(Math.random() *10 + 5)
+        const rows =Math.floor(Math.random() * 5 + 2)
+
+        this.width =columns * 70
+        for(let x=0; x<columns; x++){
+            for(let y=0; y<rows; y++){
+            this.virus.push(new Virus({position:{
+                x:x * 70,
+                y:y*50
+            }}))
+        }}
+        console.log(this.virus)
+    }
+ 
+    update() {
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        this.velocity.y =0
+
+        if(this.position.x + this.width >= canvas.width || this.position.x <= 0){
+            this.velocity.x = -this.velocity.x
+            this. velocity.y=25
+        }
+    }
+}
+
+
 const player=new Player()
+const projectiles =[]
+const grids=[]
 const keys={
     a:{
         pressed:false
@@ -75,6 +171,8 @@ const keys={
 }
 player.draw()
 
+let frames = 0
+let randomInterval =Math.floor((Math.random() * 500) + 500)
 function animate(){
     requestAnimationFrame(animate)
     context.fillStyle='black'
@@ -82,6 +180,41 @@ function animate(){
     // console.log('default')
     // player.draw()
     player.update()
+    projectiles.forEach((projectile,index) =>{
+
+        if (projectile.position.y + projectile.radius <=0){
+            setTimeout(() =>{
+            projectiles.splice(index, 1)
+            },0)
+        }
+        else{
+            projectile.update()
+        }
+    })
+
+    grids.forEach((grid) => {
+        grid.update()
+        grid.virus.forEach((virus,i) =>{
+            virus.update({velocity: grid.velocity})
+
+            projectiles.forEach((projectile, j) =>{
+                if(projectile.position.y - projectile.radius <= virus.position.y + virus.height && projectile.position.x + projectile.radius >= virus.position.x && projectile.position.x - projectile.radius <= virus.position.x + virus.width && projectile.position.y + projectile.radius >= virus.position.y ){
+                    setTimeout(() =>{
+                        const virusFound = grid.virus.find((virus2) =>
+                            virus2 ===virus
+                        )
+                        const projectileFound = projectiles.find((projectile2) => projectile2===projectile)
+
+                        if (virusFound&&projectileFound){
+                           grid.virus.splice(i,1)
+                           projectile.splice(j,1) 
+                        }
+                        
+                    },0)
+                }
+            })
+        })
+    })
 
     if (keys.a.pressed && player.position.x >=0){
         player.velocity.x= -5
@@ -91,6 +224,13 @@ function animate(){
     else{
         player.velocity.x = 0
     }
+    if (frames % randomInterval===0){
+        grids.push(new Grid())
+        randomInterval =Math.floor((Math.random() * 500) + 500)
+        frames=0
+    }
+
+    frames++
 }
 
 animate()
@@ -108,6 +248,16 @@ addEventListener('keydown',({key})=>{
             break
         case ' ':
             console.log('space')
+            projectiles.push(new Projectile({
+                position:{
+                    x: player.position.x + player.width/2,
+                    y: player.position.y
+                },
+                velocity:{
+                    x:0,
+                    y:-10
+                }
+            }))
             break
     }
 })
